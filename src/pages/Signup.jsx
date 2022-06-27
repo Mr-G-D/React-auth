43478101d679d0f198986276da1995414aa0b21c
Mr-G-D <daredinesh00@gmail.com>
2022-06-27 15:16:57 +0530

    :hammer: initiating signup page

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const Signup = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: 'url("/bg.png")',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      Signup
+    </div>
+  );
+};
+
+export default Signup;
